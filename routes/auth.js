10945116ddbbbf39574598c5c5a55efb55bd5fbd
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-04-10 12:24:12 +0200

    Login page created

--- routes/auth.js
+++ routes/auth.js
@@ -8,4 +8,12 @@ router.post("/signup", (req, res, next) => {
   console.log("The user data:", req.body);
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
+  console.log("The user data:", req.body);
+});
+
 module.exports = router;
